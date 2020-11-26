ceda0bc238bebc495501d98e111dbb06342ed72a
duanhan <duanhan94@163.com>
2020-11-26 10:54:43 +0800

    moreLayer

--- main.py
+++ main.py
@@ -3,13 +3,4 @@ from CNNsDemo import *
 
 # train()
 # acc_cal(100)
-# display(10)
-# print(torch.cuda.is_available())
-# print("git test")
-#ceshi ceshi
-# print("ceshiceshi")
-
-
-##this is moreLayer
-wcibsjcjscbj
-wcwsce
+display(10)
